b848c8255b9e2cc7e96a3b2f28b4b98dddbf8cb4
Thomas Eusebio Vieira <thomas.eusebio.vieira@egmail.com>
2021-09-18 15:52:55 -0300

    feat: implement filters by level, age and message text and add json endpoint

--- src/routes/log.routes.ts
+++ src/routes/log.routes.ts
@@ -27,16 +27,70 @@ logRouter.post('/', ensureAuthenticated, async (request, response) => {
 });
 
 logRouter.get('/', async (request, response) => {
-  const { from } = request.query;
+  const { level, age, contains } = request.query;
   let resultado;
   let logCollection;
+  let levelCondition;
+  let ageInMilliseconds;
+  let ageCondition;
+  let containsCondition;
+
+  switch (level) {
+    case 'justwarn':
+      levelCondition = 'warn';
+      break;
+    case 'justinfo':
+      levelCondition = 'info';
+      break;
+    case 'justverbose':
+      levelCondition = 'verbose';
+      break;
+    case 'justdebug':
+      levelCondition = 'debug';
+      break;
+    case 'justerror':
+    case 'error':
+      levelCondition = 'error';
+      break;
+    case 'warn':
+      levelCondition = { $in: ['error', 'warn'] };
+      break;
+    case 'info':
+      levelCondition = { $in: ['error', 'warn', 'info'] };
+      break;
+    case 'verbose':
+      levelCondition = { $in: ['error', 'warn', 'info', 'verbose'] };
+      break;
+    case 'debug':
+    default:
+      levelCondition = { $in: ['error', 'warn', 'info', 'verbose', 'debug'] };
+  }
+
+  if (typeof age === 'string') {
+    ageInMilliseconds = parseInt(age, 10) * 60 * 60 * 1000; // horas * minutos * segundos * milisegundos
+  } else {
+    ageInMilliseconds = 24 * 60 * 60 * 1000; // horas * minutos * segundos * milisegundos
+  }
+  ageCondition = Date.now() - ageInMilliseconds;
+
+  const regex = `^.*${contains}.*$`;
+
+  if (!contains) {
+    containsCondition = { $regex: `^(?!\s*$).+` };
+  } else {
+    containsCondition = { $regex: regex };
+  }
 
   const client = await connection();
 
   try {
     logCollection = client.db('logserver').collection('logs');
     resultado = await logCollection
-      .find({ createdAt: { $gt: new Date('2021-09-16T12:00:00.000-03:00') } })
+      .find({
+        level: levelCondition,
+        createdAt: { $gt: new Date(ageCondition) },
+        message: containsCondition,
+      })
       .sort({ createdAt: 1 })
       .toArray();
   } finally {
@@ -44,11 +98,19 @@ logRouter.get('/', async (request, response) => {
   }
 
   var pagina = `<html>
-      <head>Log Server</head>
+      <head>
+        <title>Log Server</title>
+      </head>
       <body>
         <h1>
           Lista de Logs
         </h1>
+        <h3>
+          Filtros: level, age, contains
+        </h3>
+        <h4>
+          Ex.: http://localhost:3333/log?level=info&age=24&contains=Data
+        </h4>
         <ul>
         `;
   resultado.forEach(log => {
@@ -63,6 +125,80 @@ logRouter.get('/', async (request, response) => {
     </html>`;
 
   return response.send(pagina);
+});
+
+logRouter.get('/json', async (request, response) => {
+  const { level, age, contains } = request.query;
+  let resultado;
+  let logCollection;
+  let levelCondition;
+  let ageInMilliseconds;
+  let ageCondition;
+  let containsCondition;
+
+  switch (level) {
+    case 'justwarn':
+      levelCondition = 'warn';
+      break;
+    case 'justinfo':
+      levelCondition = 'info';
+      break;
+    case 'justverbose':
+      levelCondition = 'verbose';
+      break;
+    case 'justdebug':
+      levelCondition = 'debug';
+      break;
+    case 'justerror':
+    case 'error':
+      levelCondition = 'error';
+      break;
+    case 'warn':
+      levelCondition = { $in: ['error', 'warn'] };
+      break;
+    case 'info':
+      levelCondition = { $in: ['error', 'warn', 'info'] };
+      break;
+    case 'verbose':
+      levelCondition = { $in: ['error', 'warn', 'info', 'verbose'] };
+      break;
+    case 'debug':
+    default:
+      levelCondition = { $in: ['error', 'warn', 'info', 'verbose', 'debug'] };
+  }
+
+  if (typeof age === 'string') {
+    ageInMilliseconds = parseInt(age, 10) * 60 * 60 * 1000; // horas * minutos * segundos * milisegundos
+  } else {
+    ageInMilliseconds = 24 * 60 * 60 * 1000; // horas * minutos * segundos * milisegundos
+  }
+  ageCondition = Date.now() - ageInMilliseconds;
+
+  const regex = `^.*${contains}.*$`;
+
+  if (!contains) {
+    containsCondition = { $regex: `^(?!\s*$).+` };
+  } else {
+    containsCondition = { $regex: regex };
+  }
+
+  const client = await connection();
+
+  try {
+    logCollection = client.db('logserver').collection('logs');
+    resultado = await logCollection
+      .find({
+        level: levelCondition,
+        createdAt: { $gt: new Date(ageCondition) },
+        message: containsCondition,
+      })
+      .sort({ createdAt: 1 })
+      .toArray();
+  } finally {
+    //await client.close();
+  }
+
+  return response.json(resultado);
 });
 
 export default logRouter;
